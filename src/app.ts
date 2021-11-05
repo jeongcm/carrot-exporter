1afa008cfe05101409e24adc4bed75fb75aa436f
Jaswant Singh <jaswant.singh@exubers.com>
2021-11-05 14:53:55 +0530

    project setup,logs settings

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ class App {
     this.app.listen(this.port, () => {
       logger.info(`=================================`);
       logger.info(`======= ENV: ${this.env} =======`);
-      logger.info(`🚀 App listening on the port ${this.port}`);
+      logger.info(`🚀 Nexclipper listening on the port ${this.port}`);
       logger.info(`=================================`);
     });
   }
