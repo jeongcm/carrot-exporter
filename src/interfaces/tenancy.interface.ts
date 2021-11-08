ee708d7ad968dc92cab86fabf0dfca3fac33571b
shrishtiExubers <shrishti.raj@exubers.com>
2021-11-08 12:31:49 +0530

    user, tenancy interface

---
+++ src/interfaces/tenancy.interface.ts
@@ -0,0 +1,12 @@
+export interface Tenancy {
+
+    "id": number,
+    "prdCode": string
+    "tenancyCode": string
+    "tenancyName": number,
+    "tenancyDescription": true,
+    "createdAt": Date,
+    "updatedAt": Date
+
+
+}
---
+++ src/interfaces/tenancyMember.interface.ts
@@ -0,0 +1,15 @@
+export interface TenancyMember {
+
+    "id": number,
+    "user": string
+    "userId": string
+    "tenancyId": number,
+    "isActivated": true,
+    "isSelected": boolean,
+    "isDeleted": boolean,
+    "tenancyLastAccess": Date,
+    "createdAt": Date,
+    "updatedAt": Date
+
+
+}
