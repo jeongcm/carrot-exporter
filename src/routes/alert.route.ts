9ddf538d9b2ef96f393bd7d2874182e25ef37720
yoosaemsol <yoosaemsol@gmail.com>
2021-11-25 17:17:37 +0900

    Add Authentication into Alert APIs

--- src/routes/alert.route.ts
+++ src/routes/alert.route.ts
@@ -3,21 +3,23 @@ import { Routes } from '@interfaces/routes.interface';
 import AlertController from '@/controllers/alert.controller';
 import validationMiddleware from '@middlewares/validation.middleware';
 import { CreateAlertDto } from '@dtos/alert.dto';
+import AuthService from '@/services/auth.service';
 
 class AlertRoute implements Routes {
   public path = '/alerts';
   public router = Router();
   public alertController = new AlertController();
+  public authservice = new AuthService();
 
   constructor() {
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.alertController.getAlerts);
-    this.router.get(`${this.path}/:id`, this.alertController.getAlert);
-    this.router.post(`${this.path}`, validationMiddleware(CreateAlertDto, 'body'), this.alertController.createAlert);
-    this.router.delete(`${this.path}/:id`, this.alertController.deleteAlert);
+    this.router.get(`${this.path}`, this.authservice.authenticate, this.alertController.getAlerts);
+    this.router.get(`${this.path}/:id`, this.authservice.authenticate, this.alertController.getAlert);
+    this.router.post(`${this.path}`, this.authservice.authenticate, validationMiddleware(CreateAlertDto, 'body'), this.alertController.createAlert);
+    this.router.delete(`${this.path}/:id`, this.authservice.authenticate, this.alertController.deleteAlert);
   }
 }
 
