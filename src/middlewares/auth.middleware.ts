8cf702b620456012ac9d9848f990b5741f42a067
shrishtiExubers <shrishti.raj@exubers.com>
2021-12-16 09:26:07 +0530

    console removed

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -10,16 +10,13 @@ const authMiddleware = async (req: RequestWithUser, res: Response, next: NextFun
     const Authorization = req.cookies['X-AUTHORIZATION'] || req.header('x-authorization').split('Bearer ')[1] || null;
 
     if (Authorization) {
-      console.log("Authorization", Authorization, req.cookies['X-AUTHORIZATION'] )
       const secretKey: string = config.get('secretKey');
       const verificationResponse = jwt.verify(Authorization, secretKey) as DataStoredInToken;
       const userId = verificationResponse.id;
-      console.log("userId", userId);
       const findUser = await DB.Users.findByPk(userId);
 
       if (findUser) {
-        console.log("findUser", findUser)
-        req["user"] = findUser;
+        req.user = findUser;
         next();
       } else {
         next(new HttpException(401, 'Wrong authentication token'));
