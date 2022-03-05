29963a0b129814a0a57301735a76b8a9c2342ce9
Ryan Lee <askryanlee@gmail.com>
2022-03-05 03:19:24 -0800

    add production & development validation

--- src/config/validateEnv.ts
+++ src/config/validateEnv.ts
@@ -3,30 +3,49 @@ import { cleanEnv, email, host, num, port, str } from 'envalid';
  * @method  validate a particular env.
  */
 function validateEnv() {
-  cleanEnv(process.env, {
-    NC_LARI_PORT: port(),
-    NC_LARI_ENV: str(),
-    NC_LARI_DB_CONFIG_HOST: host(),
-    NC_LARI_DB_CONFIG_USER: str(),
-    NC_LARI_DB_CONFIG_PASSWORD: str(),
-    NC_LARI_DB_CONFIG_DB_NAME: str(),
-    NC_LARI_DB_CONFIG_POOL_MIN: num(),
-    NC_LARI_DB_CONFIG_POOL_MAX: num(),
-    NC_LARI_EMAIL_DEFAULT_FROM: email(),
-    NC_LARI_MAILGUN_API_KEY: str(),
-    NC_LARI_MAILGUN_DOMAIN: host(),
-    NC_LARI_SOCIALKEY_GOOGLE_CLIENT_ID: str(),
-    NC_LARI_SOCIALKEY_GOOGLE_CLIENT_SECRET: str(),
-    NC_LARI_SOCIALKEY_GOOGLE_CALLBACK_URL: host(),
-    NC_LARI_SOCIALKEY_GITHUB_CLIENT_ID: str(),
-    NC_LARI_SOCIALKEY_GITHUB_CLIENT_SECRET: str(),
-    NC_LARI_SOCIALKEY_GITHUB_CALLBACK_URL: host(),
-    NC_LARI_JWT_SECRET_KEY: str(),
-    NC_LARI_LOG_FORMAT: str(),
-    NC_LARI_CORS_ORIGIN: str(),
-    NC_LARI_CORS_CREDENTIALS: str(),
-    NC_LARI_EMAIL_PASSWORD_RESET_PAGE_URL: host(),
-  });
+  if (process.env.NC_LARI_ENV === 'production') {
+    cleanEnv(process.env, {
+      NC_LARI_PORT: port(),
+      NC_LARI_ENV: str(),
+      NC_LARI_DB_CONFIG_HOST: str(),
+      NC_LARI_DB_CONFIG_USER: str(),
+      NC_LARI_DB_CONFIG_PASSWORD: str(),
+      NC_LARI_DB_CONFIG_DB_NAME: str(),
+      NC_LARI_DB_CONFIG_POOL_MIN: num(),
+      NC_LARI_DB_CONFIG_POOL_MAX: num(),
+      NC_LARI_EMAIL_DEFAULT_FROM: email(),
+      NC_LARI_MAILGUN_API_KEY: str(),
+      NC_LARI_MAILGUN_DOMAIN: host(),
+      NC_LARI_SOCIALKEY_GOOGLE_CLIENT_ID: str(),
+      NC_LARI_SOCIALKEY_GOOGLE_CLIENT_SECRET: str(),
+      NC_LARI_SOCIALKEY_GOOGLE_CALLBACK_URL: str(),
+      NC_LARI_SOCIALKEY_GITHUB_CLIENT_ID: str(),
+      NC_LARI_SOCIALKEY_GITHUB_CLIENT_SECRET: str(),
+      NC_LARI_SOCIALKEY_GITHUB_CALLBACK_URL: str(),
+      NC_LARI_JWT_SECRET_KEY: str(),
+      NC_LARI_LOG_FORMAT: str(),
+      NC_LARI_CORS_ORIGIN: str(),
+      NC_LARI_CORS_CREDENTIALS: str(),
+      NC_LARI_EMAIL_PASSWORD_RESET_PAGE_URL: str(),
+    });
+  } else {
+    cleanEnv(process.env, {
+      NC_LARI_PORT: port(),
+      NC_LARI_ENV: str(),
+      NC_LARI_DB_CONFIG_HOST: str(),
+      NC_LARI_DB_CONFIG_USER: str(),
+      NC_LARI_DB_CONFIG_PASSWORD: str(),
+      NC_LARI_DB_CONFIG_DB_NAME: str(),
+      NC_LARI_DB_CONFIG_POOL_MIN: num(),
+      NC_LARI_DB_CONFIG_POOL_MAX: num(),
+      NC_LARI_EMAIL_DEFAULT_FROM: email(),
+      NC_LARI_JWT_SECRET_KEY: str(),
+      NC_LARI_LOG_FORMAT: str(),
+      NC_LARI_CORS_ORIGIN: str(),
+      NC_LARI_CORS_CREDENTIALS: str(),
+      NC_LARI_EMAIL_PASSWORD_RESET_PAGE_URL: str(),
+    });
+  }
 }
 
 export default validateEnv;
