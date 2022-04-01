5c521a2baff386dd34d9840e71b71fcbf7d25f3e
akshay <akshay.tiwari@exubers.com>
2022-04-01 09:50:00 +0530

    added channel interface

--- src/common/interfaces/channel.interface.ts
+++ src/common/interfaces/channel.interface.ts
@@ -54,17 +54,3 @@ export interface Channel {
   channelAdaptor: string;
 }
 
-export interface PartyChannel {
-  partyChannelKey: number;
-  partyKey: number;
-  channelKey: number;
-  PartychannelId: string;
-  createdBy: string;
-  updatedBy: string;
-  createdAt: Date;
-  updatedAt: Date;
-  isDeleted: boolean;
-  partyChannelFrom: Date;
-  partyChannelTo: Date;
-  partyChannelDefault: boolean;
-}
