d391d502ea1c775c9d5bafaa8333610c60ce6083
yoosaemsol <yoosaemsol@gmail.com>
2022-04-01 15:16:30 +0900

    Party (AccessGroup) API Implementation.

--- src/modules/Party/routes/party.route.ts
+++ src/modules/Party/routes/party.route.ts
@@ -3,7 +3,7 @@ import { Routes } from '@/common/interfaces/routes.interface';
 
 import AuthService from '@/modules/UserTenancy/services/auth.service';
 import authMiddleware from '@/modules/ApiGateway/middlewares/auth.middleware';
-import { CreateUserDto, UpdateUserDto, LoginDto } from '@/modules/Party/dtos/party.dto';
+import { CreateUserDto, UpdateUserDto, CreateAccessGroupDto, AddUserAccessGroupDto, LoginDto } from '@/modules/Party/dtos/party.dto';
 import validationMiddleware from '@/common/middlewares/validation.middleware';
 
 import PartyController from '../controllers/party.controller';
@@ -22,10 +22,38 @@ class tableIdRoute implements Routes {
     this.router.get('/logout', this.partyController.logout);
 
     this.router.post('/party/user', systemAuthMiddleware, validationMiddleware(CreateUserDto, 'body'), this.partyController.createUser);
-
     this.router.get('/party/user', authMiddleware, this.partyController.getUsers);
     this.router.get('/party/user/:partyUserId', authMiddleware, this.partyController.getUser);
     this.router.put('/party/user/:partyUserId', authMiddleware, validationMiddleware(UpdateUserDto, 'body'), this.partyController.updateUser);
+
+    this.router.post(
+      '/party/accessgroup',
+      authMiddleware,
+      validationMiddleware(CreateAccessGroupDto, 'body'),
+      this.partyController.createAccessGroup,
+    );
+    this.router.get('/party/accessgroup', authMiddleware, this.partyController.getAccessGroups);
+    this.router.get('/party/accessgroup/:partyId', authMiddleware, this.partyController.getAccessGroup);
+    this.router.put(
+      '/party/accessgroup/:partyId',
+      authMiddleware,
+      validationMiddleware(CreateAccessGroupDto, 'body'),
+      this.partyController.updateAccessGroup,
+    );
+
+    this.router.post(
+      '/party/accessgroup/:partyId/users',
+      authMiddleware,
+      validationMiddleware(AddUserAccessGroupDto, 'body'),
+      this.partyController.addUserToAccessGroup,
+    );
+    this.router.get('/party/accessgroup/:partyId/users', authMiddleware, this.partyController.getUserOfAccessGroup);
+    this.router.delete(
+      '/party/accessgroup/:partyId/users',
+      authMiddleware,
+      validationMiddleware(AddUserAccessGroupDto, 'body'),
+      this.partyController.removeUserFromAccessGroup,
+    );
   }
 }
 
