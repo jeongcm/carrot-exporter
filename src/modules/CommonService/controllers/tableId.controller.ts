928046856862c466ead361318d783d6ac480ea84
Jerry Lee <jerry.lee@nexclipper.io>
2022-04-08 04:10:46 -0700

    add a new service - issueTableIdBult for alert and metrics

--- src/modules/CommonService/controllers/tableId.controller.ts
+++ src/modules/CommonService/controllers/tableId.controller.ts
@@ -1,13 +1,13 @@
 import { NextFunction, Request, Response } from 'express';
-import { IssueTableIdDto, IResponseIssueTableIdDto } from '@/modules/CommonService/dtos/tableId.dto';
-import { ITableId as TableId } from '@/common/interfaces/tableId.interface';
+import { IResponseIssueTableIdDto, IResponseIssueTableIdBulkDto } from '@/modules/CommonService/dtos/tableId.dto';
+//import { ITableId as TableId } from '@/common/interfaces/tableId.interface';
 import tableIdService from '@/modules/CommonService/services/tableId.service';
-import { RequestWithUser } from '@/common/interfaces/auth.interface';
+import { IRequestWithUser } from '@/common/interfaces/party.interface';
 
 class TableIdController {
   public tableIdService = new tableIdService();
 
-  public issueTableId = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+  public issueTableId = async (req: IRequestWithUser, res: Response, next: NextFunction) => {
     try {
       const tableIdTableName = req.body.tableName;
 
@@ -23,6 +23,27 @@ class TableIdController {
       next(error);
     }
   };
+
+  public issueTableIdBulk = async (req: IRequestWithUser, res: Response, next: NextFunction) => {
+    try {
+      const tableIdTableName = req.body.tableName;
+      const tableIdKeyRange = req.body.tableIdRange;
+
+      const tableId = await this.tableIdService.getTableIdByTableName(tableIdTableName);
+
+      if (!tableId) {
+        return res.sendStatus(404);
+      }
+
+      const responseTableIdData: IResponseIssueTableIdBulkDto = await this.tableIdService.issueTableIdBulk(tableIdTableName, tableIdKeyRange);
+      res.status(200).json({ data: responseTableIdData, message: `new table IDs are issued for ${tableIdTableName} table` });
+      
+
+    } catch(error) {
+      next(error);
+    }
+  };
+
 }
 
 export default TableIdController;
