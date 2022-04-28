dc82b048a99af13dfaac766f5a7937f6caa3c0d8
yoosaemsol <yoosaemsol@gmail.com>
2022-04-28 11:19:29 +0900

    partyUserLogAPIs implementation.

---
+++ src/modules/ApiGateway/middlewares/createUserLogMiddleware.ts
@@ -0,0 +1,51 @@
+import { NextFunction, Request, Response } from 'express';
+
+import DB from '@/database';
+
+import { IResponseIssueTableIdDto } from '@/modules/CommonService/dtos/tableId.dto';
+import TableIdService from '@/modules/CommonService/services/tableId.service';
+import { IPartyUser } from '@/common/interfaces/party.interface';
+import { IApi } from '@/common/interfaces/api.interface';
+
+/**
+ * Middleware to be used to authenticate a particular request.
+ * @param  {} req
+ * @param  {Response} res
+ * @param  {NextFunction} next
+ */
+
+const createUserLogMiddleware = async (req, res: Response, next: NextFunction) => {
+  const tableIdService = new TableIdService();
+
+  const partyUserId = req?.user?.partyId ? req?.user?.partyId : req?.systemId;
+  const apiEndPoint1 = req.method;
+  const apiEndPoint2 = req?.route?.path;
+
+  const findPartyUser: IPartyUser = await DB.PartyUser.findOne({
+    where: {
+      partyUserId,
+    },
+  });
+
+  const findApi: IApi = await DB.Api.findOne({
+    where: {
+      apiEndPoint1,
+      apiEndPoint2,
+    },
+  });
+
+  try {
+    const responseTableIdData: IResponseIssueTableIdDto = await tableIdService.issueTableId('PartyUserLogs');
+
+    await DB.PartyUserLogs.create({
+      partyUserLogsId: responseTableIdData.tableIdFinalIssued,
+      partyUserKey: findPartyUser.partyUserKey,
+      apiKey: findApi.apiKey,
+      createdBy: partyUserId,
+    });
+
+    return next();
+  } catch (error) {}
+};
+
+export default createUserLogMiddleware;
--- src/modules/Party/routes/party.route.ts
+++ src/modules/Party/routes/party.route.ts
@@ -15,6 +15,7 @@ import validationMiddleware from '@/common/middlewares/validation.middleware';
 
 import PartyController from '../controllers/party.controller';
 import systemAuthMiddleware from '@/modules/ApiGateway/middlewares/systemAuth.middleware';
+import createUserLogMiddleware from '@/modules/ApiGateway/middlewares/createUserLogMiddleware';
 
 class partyRoute implements Routes {
   public router = Router();
@@ -25,26 +26,40 @@ class partyRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.post('/login', validationMiddleware(LoginDto, 'body'), this.partyController.login);
-    this.router.get('/logout', this.partyController.logout);
+    this.router.post('/login', systemAuthMiddleware, validationMiddleware(LoginDto, 'body'), createUserLogMiddleware, this.partyController.login);
+    this.router.get('/logout', systemAuthMiddleware, createUserLogMiddleware, this.partyController.logout);
 
-    this.router.post('/party/user', systemAuthMiddleware, validationMiddleware(CreateUserDto, 'body'), this.partyController.createUser);
-    this.router.get('/party/user', authMiddleware, this.partyController.getUsers);
-    this.router.get('/party/user/:partyUserId', authMiddleware, this.partyController.getUser);
-    this.router.put('/party/user/:partyUserId', authMiddleware, validationMiddleware(UpdateUserDto, 'body'), this.partyController.updateUser);
+    this.router.post(
+      '/party/user',
+      systemAuthMiddleware,
+      validationMiddleware(CreateUserDto, 'body'),
+      createUserLogMiddleware,
+      this.partyController.createUser,
+    );
+    this.router.get('/party/user', authMiddleware, createUserLogMiddleware, this.partyController.getUsers);
+    this.router.get('/party/user/:partyUserId', authMiddleware, createUserLogMiddleware, this.partyController.getUser);
+    this.router.put(
+      '/party/user/:partyUserId',
+      authMiddleware,
+      validationMiddleware(UpdateUserDto, 'body'),
+      createUserLogMiddleware,
+      this.partyController.updateUser,
+    );
 
     this.router.post(
       '/party/accessgroup',
       authMiddleware,
       validationMiddleware(CreateAccessGroupDto, 'body'),
+      createUserLogMiddleware,
       this.partyController.createAccessGroup,
     );
-    this.router.get('/party/accessgroup', authMiddleware, this.partyController.getAccessGroups);
-    this.router.get('/party/accessgroup/:partyId', authMiddleware, this.partyController.getAccessGroup);
+    this.router.get('/party/accessgroup', authMiddleware, createUserLogMiddleware, this.partyController.getAccessGroups);
+    this.router.get('/party/accessgroup/:partyId', authMiddleware, createUserLogMiddleware, this.partyController.getAccessGroup);
     this.router.put(
       '/party/accessgroup/:partyId',
       authMiddleware,
       validationMiddleware(CreateAccessGroupDto, 'body'),
+      createUserLogMiddleware,
       this.partyController.updateAccessGroup,
     );
 
@@ -52,13 +67,15 @@ class partyRoute implements Routes {
       '/party/accessgroup/:partyId/users',
       authMiddleware,
       validationMiddleware(AddUserAccessGroupDto, 'body'),
+      createUserLogMiddleware,
       this.partyController.addUserToAccessGroup,
     );
-    this.router.get('/party/accessgroup/:partyId/users', authMiddleware, this.partyController.getUserOfAccessGroup);
+    this.router.get('/party/accessgroup/:partyId/users', authMiddleware, createUserLogMiddleware, this.partyController.getUserOfAccessGroup);
     this.router.delete(
       '/party/accessgroup/:partyId/users',
       authMiddleware,
       validationMiddleware(AddUserAccessGroupDto, 'body'),
+      createUserLogMiddleware,
       this.partyController.removeUserFromAccessGroup,
     );
 
@@ -66,15 +83,18 @@ class partyRoute implements Routes {
       '/party/accessgroup/:partyId/resource',
       authMiddleware,
       validationMiddleware(AddResourceToAccessGroupDto, 'body'),
+      createUserLogMiddleware,
       this.partyController.addResourceToAccessGroup,
     );
     this.router.delete(
       '/party/accessgroup/:partyId/resource',
       authMiddleware,
       validationMiddleware(AddResourceToAccessGroupDto, 'body'),
+      createUserLogMiddleware,
       this.partyController.removeResourceFromAccessGroup,
     );
-    this.router.get('/party/accessgroup/:partyId/resource', authMiddleware, this.partyController.getResourceOfAccessGroup);
+    this.router.get('/party/accessgroup/:partyId/resource', authMiddleware, createUserLogMiddleware, this.partyController.getResourceOfAccessGroup);
+    this.router.get('/party/:partyId/apilog', authMiddleware, createUserLogMiddleware, this.partyController.getUserAPILog);
   }
 }
 
