ed39d759459fd73239b8fabd8dbf764caf3e28e9
Jaswant Singh <93500019+jaswantexubers@users.noreply.github.com>
2022-04-29 15:54:38 +0530

    wip

--- src/tests/api.test.ts
+++ src/tests/api.test.ts
@@ -1,164 +1,49 @@
 import { Sequelize } from 'sequelize';
 import request from 'supertest';
+const bodyParser = require("body-parser");
 import App from '@/app';
+import { CreateCatalogPlanDto } from '@/modules/ProductCatalog/dtos/productCatalog.dto';
+import ProductCatalogRoute from '@/modules/ProductCatalog/routes/productCatalog.route';
+import authMiddleware from '@/modules/ApiGateway/middlewares/auth.middleware';
+const jwt = require('jsonwebtoken');
+
+import { cleanEnv, email, host, num, port, str } from 'envalid';
 import ApiRoute from '@/modules/Api/routes/api.route';
 import { ApiDto } from '@/modules/Api/dtos/api.dto';
+
 
 afterAll(async () => {
   await new Promise<void>(resolve => setTimeout(() => resolve(), 500));
 });
 
-describe('Testing API Module', () => {
-  describe('[GET] All apis /api', () => {
-    it('response findAll apis', async () => {
-      const apiRoute = new ApiRoute();
-      const apis = apiRoute.apiController.apiService.api;
+describe('Sample Test', () => {
+  it('should test that true === true', () => {
+    expect(true).toBe(true)
+  })
+})
 
-      apis.findAll = jest.fn().mockReturnValue([
-        {
-          apiId: 'AP24052700000001',
-          createdBy: 'PU24052700000002',
-          updatedBy: null,
-          createdAt: '2022-04-26T17:09:30.000Z',
-          updatedAt: '2022-04-26T17:09:30.000Z',
-          apiName: 'example 1',
-          apiDescription: 'example 1',
-          apiEndPoint1: 'example 1',
-          apiEndPoint2: 'example 1',
-          apiVisibleTF: true,
-        },
-        {
-          apiId: 'AP24052700000002',
-          createdBy: 'PU24052700000002',
-          updatedBy: null,
-          createdAt: '2022-04-26T17:09:32.000Z',
-          updatedAt: '2022-04-26T17:09:32.000Z',
-          apiName: 'example 1',
-          apiDescription: 'example 1',
-          apiEndPoint1: 'example 1',
-          apiEndPoint2: 'example 1',
-          apiVisibleTF: true,
-        },
-        {
-          apiId: 'AP24052700000003',
-          createdBy: 'PU24052700000002',
-          updatedBy: null,
-          createdAt: '2022-04-26T17:09:33.000Z',
-          updatedAt: '2022-04-26T17:09:33.000Z',
-          apiName: 'example 1',
-          apiDescription: 'example 1',
-          apiEndPoint1: 'example 1',
-          apiEndPoint2: 'example 1',
-          apiVisibleTF: true,
-        },
-        {
-          apiId: 'AP24052700000004',
-          createdBy: 'PU24052700000002',
-          updatedBy: null,
-          createdAt: '2022-04-26T17:09:34.000Z',
-          updatedAt: '2022-04-26T17:09:34.000Z',
-          apiName: 'example 1',
-          apiDescription: 'example 1',
-          apiEndPoint1: 'example 1',
-          apiEndPoint2: 'example 1',
-          apiVisibleTF: true,
-        },
-      ]);
+describe('Post Endpoints', () => {
+  test('[POST] /api', async () => {
+    const jwtSpy = jest.spyOn(jwt, 'verify');
+    jwtSpy.mockReturnValue('Some decoded token');
+    const apiRoute = new ApiRoute();
+    const api = apiRoute.apiController.apiService.api;
+    const requestPayload = {
+      apiName: 'example 1',
+      apiDescription: 'example 1',
+      apiEndPoint1: 'example 1',
+      apiEndPoint2: 'example 1',
+      apiVisibleTF: true,
+    };
+  (Sequelize as any).authenticate = jest.fn();
+  const app = new App([apiRoute]);
+  api.create = jest.fn().mockReturnValue({...requestPayload});
+  const res = await request(app.getServer()).post('/api').send(requestPayload)
+    // .set('x-authorization', `Bearer ${request.token}`) //Authorization token
+    expect(res.statusCode).toEqual(201)
+  })
+})
 
-      (Sequelize as any).authenticate = jest.fn();
-      const app = new App([apiRoute]);
-      return request(app.getServer()).get(`${apiRoute.router.get}`).expect(200);
-    });
-  });
 
-  describe('[GET] API /api/:id', () => {
-    it('response findOne api', async () => {
-      const apiId = 'AP24052700000001';
-
-      const apiRoute = new ApiRoute();
-      const api = apiRoute.apiController.apiService.api;
-
-      api.findByPk = jest.fn().mockReturnValue({
-        apiId: 'AP24052700000001',
-        createdBy: 'PU24052700000002',
-        updatedBy: null,
-        createdAt: '2022-04-26T17:09:30.000Z',
-        updatedAt: '2022-04-26T17:09:30.000Z',
-        apiName: 'example 1',
-        apiDescription: 'example 1',
-        apiEndPoint1: 'example 1',
-        apiEndPoint2: 'example 1',
-        apiVisibleTF: true,
-      });
-
-      (Sequelize as any).authenticate = jest.fn();
-      const app = new App([apiRoute]);
-      return request(app.getServer()).get(`${apiRoute.router.get}/${apiId}`).expect(200);
-    });
-  });
-
-  describe('[POST] /api', () => {
-    it('response Create API', async () => {
-      const apiData: ApiDto = {
-        apiName: 'example 1',
-        apiDescription: 'example 1',
-        apiEndPoint1: 'example 1',
-        apiEndPoint2: 'example 1',
-        apiVisibleTF: true,
-      }
-
-      const apiRoute = new ApiRoute();
-      const api = apiRoute.apiController.apiService.api;
-
-      api.findOne = jest.fn().mockReturnValue(null);
-      api.create = jest.fn().mockReturnValue({
-        apiName: apiData.apiName,
-        apiDescription: apiData.apiDescription,
-        apiEndPoint1: apiData.apiEndPoint1,
-        apiEndPoint2: apiData.apiEndPoint2,
-        apiVisibleTF: apiData.apiVisibleTF,
-      });
-
-      (Sequelize as any).authenticate = jest.fn();
-      const app = new App([apiRoute]);
-      return request(app.getServer()).post(`${apiRoute.router}`).send(apiData).expect(201);
-    });
-  });
-
-  describe('[PUT] /api/:id', () => {
-    it('response Update api', async () => {
-      const apiId = 'AP24052700000001';
-      const apiData: ApiDto = {
-        apiName: 'example 1',
-        apiDescription: 'example 1',
-        apiEndPoint1: 'example 1',
-        apiEndPoint2: 'example 1',
-        apiVisibleTF: true,
-      }
-
-      const apiRoute = new ApiRoute();
-      const api = apiRoute.apiController.apiService.api;
-
-      api.findByPk = jest.fn().mockReturnValue({
-        apiName: apiData.apiName,
-        apiDescription: apiData.apiDescription,
-        apiEndPoint1: apiData.apiEndPoint1,
-        apiEndPoint2: apiData.apiEndPoint2,
-        apiVisibleTF: apiData.apiVisibleTF,
-      });
-      api.update = jest.fn().mockReturnValue([apiId]);
-      api.findByPk = jest.fn().mockReturnValue({
-        apiName: apiData.apiName,
-        apiDescription: apiData.apiDescription,
-        apiEndPoint1: apiData.apiEndPoint1,
-        apiEndPoint2: apiData.apiEndPoint2,
-        apiVisibleTF: apiData.apiVisibleTF,
-      });
-
-      (Sequelize as any).authenticate = jest.fn();
-      const app = new App([apiRoute]);
-      return request(app.getServer()).put(`${apiRoute.router}/${apiId}`).send(apiData).expect(200);
-    });
-  });
-
-});
+  
+  
