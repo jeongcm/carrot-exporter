f2322d9ba2b90220fa86b3d67b8ebc2744bca359
Jaswant Singh <93500019+jaswantexubers@users.noreply.github.com>
2022-05-04 00:47:07 +0530

    added middleware in api, commonCode, resource and resourceGroup Modules

--- src/modules/Resources/routes/resourceGroup.route.ts
+++ src/modules/Resources/routes/resourceGroup.route.ts
@@ -2,8 +2,9 @@ import { Router } from 'express';
 import { Routes } from '@/common/interfaces/routes.interface';
 import validationMiddleware from '@/common/middlewares/validation.middleware';
 import authMiddleware from '@/modules/ApiGateway/middlewares/auth.middleware';
-import { ResourceGroupDto} from '../dtos/resourceGroup.dto';
+import { ResourceGroupDto } from '../dtos/resourceGroup.dto';
 import ResourceGroupController from '../controllers/resourceGroup.controller';
+import createUserLogMiddleware from '@/modules/ApiGateway/middlewares/createUserLogMiddleware';
 
 class ResourceGroupRoute implements Routes {
   public router = Router();
@@ -18,16 +19,28 @@ class ResourceGroupRoute implements Routes {
       '/resourceGroup',
       authMiddleware,
       validationMiddleware(ResourceGroupDto, 'body'),
+      createUserLogMiddleware,
       this.resourceGroupController.createResourceGroup,
     );
-    this.router.get('/resourceGroup', authMiddleware, this.resourceGroupController.getAllResourceGroups);
-    this.router.get('/resourceGroup/:resourceGroupId', authMiddleware, this.resourceGroupController.getResourceGroupById);
-    this.router.get('/resourceGroup/:resourceGroupUuid', authMiddleware, this.resourceGroupController.getResourceGroupByUuid);
-    this.router.get('/resourceGroup/CustomerAccountId/:customerAccountId', authMiddleware,  this.resourceGroupController.getResourceGroupByCustomerAccountId);
+    this.router.get('/resourceGroup', authMiddleware, createUserLogMiddleware, this.resourceGroupController.getAllResourceGroups);
+    this.router.get('/resourceGroup/:resourceGroupId', authMiddleware, createUserLogMiddleware, this.resourceGroupController.getResourceGroupById);
+    this.router.get(
+      '/resourceGroup/:resourceGroupUuid',
+      authMiddleware,
+      createUserLogMiddleware,
+      this.resourceGroupController.getResourceGroupByUuid,
+    );
+    this.router.get(
+      '/resourceGroup/CustomerAccountId/:customerAccountId',
+      authMiddleware,
+      createUserLogMiddleware,
+      this.resourceGroupController.getResourceGroupByCustomerAccountId,
+    );
     this.router.put(
       '/resourceGroup/:resourceGroupId',
       authMiddleware,
       validationMiddleware(ResourceGroupDto, 'body'),
+      createUserLogMiddleware,
       this.resourceGroupController.updateResourceGroupById,
     );
   }
