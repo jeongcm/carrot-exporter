f39248794fe38f8a515c11e7d7065b2f2f33bf27
Jerry Lee <jerry.lee@nexclipper.io>
2022-06-13 21:20:11 -0700

    improve VM data feed

--- src/modules/CommonService/controllers/massUploaderMongo.controller.ts
+++ src/modules/CommonService/controllers/massUploaderMongo.controller.ts
@@ -23,28 +23,61 @@ class massUploaderMongoController{
     public massUploadVMForMetricReceived = async (req: IRequestWithSystem, res: Response, next: NextFunction) => {
       try {
 
-        const customerAccountKey = 2;
         const metricReceivedMassFeed: IMetricReceivedMassFeed = req.body;
-
         const receivedData = JSON.parse(metricReceivedMassFeed.result);
-        const clusterUuid = metricReceivedMassFeed.cluster_uuid 
+        const clusterUuid = metricReceivedMassFeed.cluster_uuid; 
         const receivedMetrics = receivedData.result;
-        
+        const message_size_mb = (Buffer.byteLength(JSON.stringify(receivedMetrics)))/1024/1024;
+        console.log("message:size:", message_size_mb);
+        if (message_size_mb>5){
+          const half = Math.ceil(receivedMetrics.length/2);
+          const firstHalf = receivedMetrics.slice(0, half); 
+          const secondHalf = receivedMetrics.slice(-half);  
+
+          let newResultMap1 = [];
+          firstHalf.map((data)=>{
+            const{metric, value} = data;
+            newResultMap1.push(JSON.stringify({metric, values: [(value[1])], timestamps:[value[0]*1000]}))
+          });
+          let finalResult1 = (newResultMap1).join("\n")
+
+          let massFeedResult1 = await this.massUploaderMongoService.massUpdoadMetricReceived(finalResult1, clusterUuid);
+          if (!massFeedResult1) {
+              return res.sendStatus(500);
+            }
+
+          let newResultMap2 = [];
+          secondHalf.map((data)=>{
+            const{metric, value} = data;
+            newResultMap2.push(JSON.stringify({metric, values: [(value[1])], timestamps:[value[0]*1000]}))
+          });
+          let finalResult2 = (newResultMap2).join("\n")
+
+          let massFeedResult2 = await this.massUploaderMongoService.massUpdoadMetricReceived(finalResult2, clusterUuid);
+          if (!massFeedResult2) {
+              return res.sendStatus(500);
+            }
+
+          res.status(200).json({ data: {...massFeedResult1, ...massFeedResult2}, message: `Bulk Metric Received feed - VM is successfully complete` });
 
 
-        let newResultMap = [];
-        receivedMetrics.map((data)=>{
-          const{metric, value} = data;
-          newResultMap.push(JSON.stringify({metric,values: [parseInt(value[1])], timestamps:[value[0]*1000]}))
-        });
-        const finalResult = (newResultMap).join("\n")
-        //console.log("new result-------\n", finalResult);
+        }
+        else {
+          let newResultMap = [];
+          receivedMetrics.map((data)=>{
+            const{metric, value} = data;
+            newResultMap.push(JSON.stringify({metric, values: [(value[1])], timestamps:[value[0]*1000]}))
+          });
+          const finalResult = (newResultMap).join("\n")
 
-        const massFeedResult = await this.massUploaderMongoService.massUpdoadMetricReceived(finalResult, clusterUuid);
-        if (!massFeedResult) {
-            return res.sendStatus(500);
-          }
-        res.status(200).json({ data: massFeedResult, message: `Bulk Metric Received feed - VM is successfully complete` });
+          const massFeedResult = await this.massUploaderMongoService.massUpdoadMetricReceived(finalResult, clusterUuid);
+          if (!massFeedResult) {
+              return res.sendStatus(500);
+            }
+          res.status(200).json({ data: massFeedResult, message: `Bulk Metric Received feed - VM is successfully complete` });
+          } //end of else 
+          
+          
       } catch (error) {
         next(error);
       }
