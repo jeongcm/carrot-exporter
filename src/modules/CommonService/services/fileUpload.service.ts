bcb8f81f9d40e687d249a4552727abdb81cf8980
Ryan Lee <askryanlee@gmail.com>
2022-06-29 15:34:32 -0700

    for debugging

--- src/modules/CommonService/services/fileUpload.service.ts
+++ src/modules/CommonService/services/fileUpload.service.ts
@@ -8,8 +8,6 @@ let space = new AWS.S3({
   credentials: new AWS.Credentials(config.fileUpload.DOAccessKeyId, config.fileUpload.DOSecretAccessKey, null),
 });
 
-console.log(config.fileUpload);
-
 const BucketName = config.fileUpload.DOBucket;
 
 class fileUploadService {
@@ -40,10 +38,13 @@ class fileUploadService {
         Key: fileName,
       };
 
-      const result = space.upload(uploadParameters);
-      var promise = result.promise();
+      console.log(uploadParameters);
+      console.log(config.fileUpload);
 
-      let data = await promise.then(
+      const result = space.upload(uploadParameters);
+      const promise = result.promise();
+
+      const data = await promise.then(
         function (data) {
           return {
             status: 'ok',
