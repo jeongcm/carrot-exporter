deed5c88f4081206ae7d04619e224d015b786d24
jerryum <84423164+jerryum@users.noreply.github.com>
2022-09-14 09:31:25 -0700

    modify the name of columns, include in creation..

--- src/modules/ProductCatalog/models/catalogPlan.model.ts
+++ src/modules/ProductCatalog/models/catalogPlan.model.ts
@@ -10,6 +10,8 @@ export type CatalogPlanCreationAttributes = Optional<
   | 'catalogPlanType'
   | 'deletedAt'
   | 'catalogPlanDescription'
+  | 'billingPlanFrequencyId'
+  | 'billingPlanId'
   | 'createdAt'
   | 'updatedAt'
   | 'createdBy'
@@ -22,8 +24,8 @@ export class CatalogPlanModel extends Model<ICatalogPlan, CatalogPlanCreationAtt
   public catalogPlanName: string;
   public catalogPlanType: string;
   public catalogPlanDescription: string;
-  public fusebillPlanFrequencyId: number;
-  public fusebillPlanId: number;
+  public billingPlanFrequencyId: number;
+  public billingPlanId: number;
   public createdBy: string;
   public updatedBy: string;
   public deletedAt: Date;
@@ -76,11 +78,11 @@ export default function (sequelize: Sequelize): typeof CatalogPlanModel {
         type: DataTypes.DATE(),
         defaultValue: new Date(),
       },
-      fusebillPlanId: {
+      billingPlanId: {
         type: DataTypes.INTEGER,
         allowNull: true,
       },
-      fusebillPlanFrequencyId: {
+      billingPlanFrequencyId: {
         type: DataTypes.INTEGER,
         allowNull: true,
       },
