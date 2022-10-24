c950c1349f038508a0ca4963d970aa8fc9389384
jeongcm <jeongcm@github.com>
2022-10-24 13:58:26 +0900

    mig/1015 getMetric for p8s api 추가

--- src/modules/Metric/services/metric.service.ts
+++ src/modules/Metric/services/metric.service.ts
@@ -8,6 +8,7 @@ import { ICustomerAccount } from 'common/interfaces/customerAccount.interface';
 import { IResourceGroup } from 'common/interfaces/resourceGroup.interface';
 import { IResource } from 'common/interfaces/resource.interface';
 import getSelectorLabels from 'common/utils/getSelectorLabels';
+import P8sService from "@modules/Metric/services/p8sService";
 
 export interface IMetricQueryBodyQuery {
   name: string;
@@ -27,6 +28,7 @@ export interface IMetricQueryBody {
 
 class MetricService extends ServiceExtension {
   private victoriaMetricService = new VictoriaMetricService();
+  private p8sService = new P8sService();
   private resourceService = new ResourceService();
   private resourceGroupService = new ResourceGroupService();
   private customerAccountService = new CustomerAccountService();
@@ -118,6 +120,119 @@ class MetricService extends ServiceExtension {
               data = await this.victoriaMetricService.queryRange(customerAccountId, `${promQl.promQl}`, `${start}`, `${end}`, step);
             } else {
               data = await this.victoriaMetricService.query(customerAccountId, `${promQl.promQl}`, step);
+            }
+
+            results[name] = {
+              ok: true,
+              data,
+              query: { ...promQl, step },
+            };
+          } catch (e) {
+            results[name] = {
+              ok: false,
+              reason: e,
+              query: { ...promQl, step },
+            };
+          }
+        }),
+      );
+    } catch (e) {
+      return this.throwError('EXCEPTION', e);
+    }
+
+    const resultInOrder = {};
+
+    queryBody.query.forEach((query: IMetricQueryBodyQuery) => {
+      const { name } = query;
+      resultInOrder[name] = results[name];
+    });
+
+    return resultInOrder;
+  }
+
+  public async getMetricP8S(customerAccountKey: number, queryBody: IMetricQueryBody) {
+    const results: any = {};
+
+    if (isEmpty(queryBody?.query)) {
+      return this.throwError('EXCEPTION', 'query[] is missing');
+    }
+
+    try {
+      await Promise.all(
+        queryBody.query.map(async (query: IMetricQueryBodyQuery) => {
+          const { name, start, end, step, resourceGroupUuid, resourceId, resourceGroupId, type } = query;
+
+          if (isEmpty(type)) {
+            return this.throwError('EXCEPTION', `type for '${name}' is missing`);
+          }
+          const customerAccountId = await this.customerAccountService.getCustomerAccountIdByKey(customerAccountKey);
+          let resources: IResource[] = null;
+          let resourceGroups: IResourceGroup[] = null;
+          if (resourceId) {
+            let idsToUse: string[] = [];
+            if (Array.isArray(resourceId)) {
+              idsToUse = resourceId;
+            } else {
+              idsToUse = [resourceId];
+            }
+            resources = await this.resourceService.getUserResourceByIds(customerAccountKey, idsToUse);
+
+            if (!resources || resources.length === 0) {
+              return this.throwError(`NOT_FOUND`, `No resource found with resourceId (${resourceId})`);
+            }
+
+            const resourceGroupKeys = resources?.map((resource: IResource) => resource.resourceGroupKey);
+
+            resourceGroups = await this.resourceGroupService.getUserResourceGroupByKeys(customerAccountKey, resourceGroupKeys);
+
+            if (!resourceGroups || resourceGroups.length === 0) {
+              return this.throwError('EXCEPTION', `No access to resourceGroups (accessed through resourceId)`);
+            }
+          } else if (resourceGroupUuid) {
+            const resourceGroup = await this.resourceGroupService.getUserResourceGroupByUuid(customerAccountKey, resourceGroupUuid);
+            if (!resourceGroup) {
+              return this.throwError('EXCEPTION', `No access to resourceGroupUuid(${resourceGroupUuid})`);
+            }
+            resourceGroups = [resourceGroup];
+          } else if (resourceGroupId) {
+            let idsToUse: string[] = [];
+            if (Array.isArray(resourceGroupId)) {
+              idsToUse = resourceGroupId;
+            } else {
+              idsToUse = [resourceGroupId];
+            }
+            resourceGroups = await this.resourceGroupService.getResourceGroupByIds(idsToUse);
+            if (!resourceGroups) {
+              return this.throwError('EXCEPTION', `No access to resourceGroupUuid(${idsToUse.join(', ')})`);
+            }
+          }
+
+          if (!resourceGroups && !resources) {
+            return this.throwError(
+              'NOT_FOUND',
+              `no resourceGroup nor resource found! Please make sure to pass resourceGroupId or resourceGroupUuid or resourceId`,
+            );
+          }
+
+          const promQl = this.getPromQlFromQuery(query, resources, resourceGroups);
+
+          if (!promQl.promQl) {
+            return this.throwError(`EXCEPTION`, `Invalid type ${type}`);
+          }
+
+          if (promQl.ranged) {
+            if (isEmpty(start) || isEmpty(end)) {
+              return this.throwError('EXCEPTION', 'start and end required for ranged query');
+            }
+          }
+
+          try {
+            let data: any = null;
+
+            if (start && end) {
+              data = await this.p8sService.queryRange(customerAccountId, `${promQl.promQl}`, `${start}`, `${end}`, step);
+            } else {
+              data = await this.p8sService.query(customerAccountId, `${promQl.promQl}`, step);
             }
 
             results[name] = {
