c344b6906854201857e72cf2fe55af349ebc8a5c
Ryan Lee <askryanlee@gmail.com>
2022-10-31 16:54:46 -0700

    Revert "improvement/1083 add .env switcher"
    This reverts commit 73e5737b99b99d61ee2395f9b6d4c06f699502fa.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import './dotenvConfig';
+import 'dotenv/config';
 import App from './app';
 import IndexRoute from '@/modules/ApiGateway/routes/index.route';
 import AccessGroupRoute from '@/modules/Party/routes/accessgroup.route';
