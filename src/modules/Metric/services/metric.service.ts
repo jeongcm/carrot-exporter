2423825617d7eb06bd2e3d7b4cd88440a3f895b6
jeongcm <jeongcm@github.com>
2022-11-01 11:41:14 +0900

    mig/1015 임시 주석

--- src/modules/Metric/services/metric.service.ts
+++ src/modules/Metric/services/metric.service.ts
@@ -1126,25 +1126,25 @@ class MetricService extends ServiceExtension {
     let length = result[metricName].data.result.length
 
     for (var i=0; i<length; i++) {
-      // get pm status
-      const statusQuery: any = {
-        query: [
-          {
-            "name": "pm_status",
-            "resourceGroupUuid": clusterUuid,
-            "type": "OS_CLUSTER_PM_NODE_STATUS",
-            "nodename": result[metricName].data.result[i].metric.nodename,
-          }
-        ]
-      }
-
-      const statusResult = await this.getMetricP8S(customerAccountKey, statusQuery)
-      let pmStatus: number
-      if (statusResult["pm_status"].data.result.length === 0) {
-        pmStatus = -2
-      } else {
-        pmStatus = statusResult["pm_status"].data.result[0].values[0]
-      }
+      // // get pm status
+      // const statusQuery: any = {
+      //   query: [
+      //     {
+      //       "name": "pm_status",
+      //       "resourceGroupUuid": clusterUuid,
+      //       "type": "OS_CLUSTER_PM_NODE_STATUS",
+      //       "nodename": result[metricName].data.result[i].metric.nodename,
+      //     }
+      //   ]
+      // }
+      //
+      // const statusResult = await this.getMetricP8S(customerAccountKey, statusQuery)
+      // let pmStatus: number
+      // if (statusResult["pm_status"].data.result.length === 0) {
+      //   pmStatus = -2
+      // } else {
+      //   pmStatus = statusResult["pm_status"].data.result[0].values[0]
+      // }
 
       uploadQuery['resource_Name'] = result[metricName].data.result[i].metric.nodename;
       uploadQuery['resource_Type'] = "PM";
