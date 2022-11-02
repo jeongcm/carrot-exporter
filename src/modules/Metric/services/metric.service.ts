ac1486aed8278488c1dfe36b4367748f8ba4d53a
jeongcm <jeongcm@github.com>
2022-11-02 21:07:36 +0900

    mig/1015 test map

--- src/modules/Metric/services/metric.service.ts
+++ src/modules/Metric/services/metric.service.ts
@@ -162,21 +162,19 @@ class MetricService extends ServiceExtension {
     const metricTypes: string[] = ["OS_CLUSTER_PM_TOTAL_CPU_COUNT", "OS_CLUSTER_PM_MEMORY_TOTAL_BYTES", "OS_CLUSTER_PM_MEMORY_USED_BYTES",
     "OS_CLUSTER_PM_FILESYSTEM_TOTAL_BYTES", "OS_CLUSTER_PM_FILESYSTEM_USED_BYTES", "OS_CLUSTER_PM_NODE_UP_TIME", "OS_CLUSTER_PM_NODE_STATUS",
     "OS_CLUSTER_PM_CPU_USAGE", "OS_CLUSTER_PM_MEMORY_USAGE", "OS_CLUSTER_PM_FILESYSTEM_USAGE"]
-
-    const results = metricTypes.map(async (metricType: string) => {
+    const promises = metricTypes.map(async metricType => {
       queryBody.query[0].type = metricType
-      const result = {}
-      result[metricType] = await this.getMetricP8S(customerAccountKey, queryBody)
+      const result = await this.getMetricP8S(customerAccountKey, queryBody).then(() => result)
       return result
     })
 
-    console.log("result:", results)
+    const results = await Promise.all(promises)
+    results.forEach(data => console.log(data))
 
     return resultList
   }
 
   public async getMetricP8S(customerAccountKey: number, queryBody: IMetricQueryBody) {
-    console.log(queryBody)
     const results: any = {};
 
     if (isEmpty(queryBody?.query)) {
