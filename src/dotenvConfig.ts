76b7fa67fe9307003c489b52cb5bda1ff109f034
Ryan Lee <askryanlee@gmail.com>
2022-11-02 11:08:51 -0700

    improvement/1083 add dotenvConfig in config

--- src/dotenvConfig.ts
+++ src/dotenvConfig.ts
@@ -4,8 +4,10 @@ import path from 'path';
 // Please change this type acccording to your own branch
 // for eg. mig_develope brach can be "mig"
 // so it will load mig.env
-const APP_DEV_TYPE = 'saas';
+const APP_DEV_TYPE = '.saas';
+
+const envPath = path.resolve(__dirname, '..', `.env${APP_DEV_TYPE || ''}`);
 
 dotenv.config({
-  path: path.resolve(__dirname, `${APP_DEV_TYPE}.env`),
+  path: path.resolve(envPath),
 });
