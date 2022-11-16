de3a66a416ddf4772ab738cc0e51ecec272f4fc3
jeongcm <jeongcm@github.com>
2022-11-16 17:03:00 +0900

    migtask/1118 pm, vm status metric 임시 추가

--- src/modules/Metric/services/metric.service.ts
+++ src/modules/Metric/services/metric.service.ts
@@ -988,7 +988,7 @@ class MetricService extends ServiceExtension {
           clusterUuid,
           nodename,
         });
-        promQl = `nc:probe_success{__LABEL_PLACE_HOLDER__}`;
+        promQl = `nc:probe_success{job=~"pm-blackbox-exporter-icmp", __LABEL_PLACE_HOLDER__}`;
         break;
 
       case 'OS_CLUSTER_PM_CPU_USAGE':
@@ -1264,9 +1264,36 @@ class MetricService extends ServiceExtension {
       case 'OS_CLUSTER_VM_NODE_STATUS':
         labelString += getSelectorLabels({
           clusterUuid,
-          nodename,
         });
-        promQl = `nc:probe_success{__LABEL_PLACE_HOLDER__}`;
+        promQl = `probe_success{job=~"vm-blackbox-exporter-icmp", __LABEL_PLACE_HOLDER__}`;
+        break;
+
+      case 'OS_CLUSTER_VM_NODE_AC01_STATUS':
+        labelString += getSelectorLabels({
+          clusterUuid,
+        });
+        promQl = `probe_success{job=~"vm-blackbox-exporter-icmp(acct-svr01)", __LABEL_PLACE_HOLDER__}`;
+        break;
+
+      case 'OS_CLUSTER_VM_NODE_AC02_STATUS':
+        labelString += getSelectorLabels({
+          clusterUuid,
+        });
+        promQl = `probe_success{job=~"vm-blackbox-exporter-icmp(acct-svr02)", __LABEL_PLACE_HOLDER__}`;
+        break;
+
+      case 'OS_CLUSTER_VM_NODE_CS01_STATUS':
+        labelString += getSelectorLabels({
+          clusterUuid,
+        });
+        promQl = `probe_success{job=~"vm-blackbox-exporter-icmp(cs_svr01)", __LABEL_PLACE_HOLDER__}`;
+        break;
+
+      case 'OS_CLUSTER_PM_NODE_PC01_STATUS':
+        labelString += getSelectorLabels({
+          clusterUuid,
+        });
+        promQl = `probe_success{job=~"pm-blackbox-exporter-icmp(p-com-02)", __LABEL_PLACE_HOLDER__}`;
         break;
     }
 
