c5b76646ce0831c68a05ac114ae8452e9b26f7ed
jeongcm <jeongcm@github.com>
2022-12-08 14:03:10 +0900

    migtask/1247 executorService 목록 조회시 date 제한 거는 부분 임시 제거

--- src/modules/CommonService/services/executor.service.ts
+++ src/modules/CommonService/services/executor.service.ts
@@ -2903,15 +2903,15 @@ class executorService {
   public async getExecutorServicebyCustomerAccountId(customerAccountId: string): Promise<IExecutorService[]> {
     const getCustomerAccount: ICustomerAccount = await this.customerAccount.findOne({ where: { deletedAt: null, customerAccountId } });
     const customerAccountKey = getCustomerAccount.customerAccountKey;
-    const dateMinus = new Date();
-    dateMinus.setDate(dateMinus.getDate() - 1);
-    const date = new Date();
-    console.log('date', date);
-    console.log('dateMinus', dateMinus);
+    // const dateMinus = new Date();
+    // dateMinus.setDate(dateMinus.getDate() - 1);
+    // const date = new Date();
+    // console.log('date', date);
+    // console.log('dateMinus', dateMinus);
 
     let getExecutorServiceAll: IExecutorService[] = await this.executorService.findAll({
       //limit: 1000,
-      where: { customerAccountKey, createdAt: { [Op.and]: { [Op.gte]: dateMinus, [Op.lte]: date } } },
+      where: { customerAccountKey, deletedAt: null },
     });
     getExecutorServiceAll = getExecutorServiceAll.sort(function (a, b) {
       const dateA = new Date(a.createdAt).getTime();
