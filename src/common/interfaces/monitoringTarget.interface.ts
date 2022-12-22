638737739808f187c4a71a079df98e999714a536
Jerry Lee <jerryum@gmail.com>
2022-12-22 09:19:48 -0800

    new table

--- src/common/interfaces/monitoringTarget.interface.ts
+++ src/common/interfaces/monitoringTarget.interface.ts
@@ -1,17 +1,16 @@
 export interface IAnomalyMonitoringTarget {
-    anomalyMonitoringTargetKey : number;
-    subscribedProductKey : number;
-    resourceKey : number;
-    bayesianModelKey : number;
-    anomalyMonitoringTargetId: string;
-    createdBy: string;
-    updatedBy: string;
-    createdAt: Date;
-    updatedAt: Date;
-    deletedAt: Date;
-    anomalyMonitoringTargetName : string;
-    anomalyMonitoringTargetDescription  : string;
-    anomalyMonitoringTargetStatus  : string;
-    customerAccountKey: number
-
+  anomalyMonitoringTargetKey: number;
+  subscribedProductKey: number;
+  resourceKey: number;
+  bayesianModelKey: number;
+  anomalyMonitoringTargetId: string;
+  createdBy: string;
+  updatedBy: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt: Date;
+  anomalyMonitoringTargetName: string;
+  anomalyMonitoringTargetDescription: string;
+  anomalyMonitoringTargetStatus: string;
+  customerAccountKey: number;
 }
