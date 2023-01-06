f1706e9c8c7e12c87ff6d38ea654da1ee6c77604
jeongcm <jeongcm@github.com>
2023-01-06 15:01:31 +0900

    migtask/1303 install kps 함수 내 resource status sync schedule 등록

--- src/modules/CommonService/services/executor.service.ts
+++ src/modules/CommonService/services/executor.service.ts
@@ -954,6 +954,17 @@ class executorService {
           throw new HttpException(500, 'Submitted kps chart installation request but fail to schedule metric-received sync');
         }); //end of catch
     }
+
+    // schedule schedule sync resource status
+    await this.scheduleSyncResourceStatus(clusterUuid)
+      .then(async (res: any) => {
+        console.log(`Submitted resource status sync schedule reqeust on ${clusterUuid} cluster successfully`);
+      })
+      .catch(error => {
+        console.log(error);
+        throw new HttpException(500, 'Submitted kps chart installation request but fail to schedule resource sync');
+      }); //end of catch
+
     //provision alert easy rule for the cluster
     const { alertEasyRule: alertEasyRuleList } = config.initialRecord;
 
@@ -1330,6 +1341,17 @@ class executorService {
           throw new HttpException(500, 'Submitted kps chart installation request but fail to schedule metric-received sync');
         }); //end of catch
     }
+
+    // schedule schedule sync resource status
+    await this.scheduleSyncResourceStatus(clusterUuid)
+      .then(async (res: any) => {
+        console.log(`Submitted resource status sync schedule reqeust on ${clusterUuid} cluster successfully`);
+      })
+      .catch(error => {
+        console.log(error);
+        throw new HttpException(500, 'Submitted kps chart installation request but fail to schedule resource sync');
+      }); //end of catch
+
     //provision alert easy rule for the cluster
     const { alertEasyRule: alertEasyRuleList } = config.initialRecord;
 
