a9f1491248bd0d2f91c7e6d6b413853617aec169
jeongcm <jeongcm@github.com>
2023-01-17 09:36:00 +0900

    migtask/1303 메트릭 조회 시 customerAccountId query 처리

--- src/modules/Metric/controllers/metric.controller.ts
+++ src/modules/Metric/controllers/metric.controller.ts
@@ -1,13 +1,22 @@
 import { NextFunction, Response } from 'express';
 import { IRequestWithUser } from '@/common/interfaces/party.interface';
 import MetricService from '../services/metric.service';
+import customerAccountService from "@modules/CustomerAccount/services/customerAccount.service";
 
 class MetricController {
   public metricService = new MetricService();
+  public customerAccountService = new customerAccountService();
 
   public getMetric = async (req: IRequestWithUser, res: Response, next: NextFunction) => {
     try {
-      const customerAccountKey = req.customerAccountKey;
+      let customerAccountKey: any
+      if (typeof req.query.customerAccountId !== "undefined") {
+        let customerAccount = await this.customerAccountService.getCustomerAccountKeyById(req.query.customerAccountId)
+        customerAccountKey = customerAccount.customerAccountKey
+      } else {
+        customerAccountKey = req.customerAccountKey
+      }
+
       const queryBody = req.body;
       const findMetricData = await this.metricService.getMetric(customerAccountKey, queryBody);
       res.status(200).json({ data: findMetricData, message: 'findAll' });
