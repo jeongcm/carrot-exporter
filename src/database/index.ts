a1168b65a856be20508c1b76c64717630b49b1a6
jeongcm <jeongcm0101@gmail.com>
2023-02-27 14:11:51 +0900

    migtask/1369 파일 이름 오타 수정

--- src/database/index.ts
+++ src/database/index.ts
@@ -58,7 +58,7 @@ import AnomalyMonitoringTargetModel from '@/modules/MetricOps/models/monitoringT
 import AnomalyMonitoringTargetResourceModel from '@/modules/MetricOps/models/monitoringTargetResource.model';
 import RoleModel from '@/modules/Role/models/role.model';
 import RolePartyModel from '@/modules/Role/models/roleParty.model';
-import ExecutorServiceModel from '@/modules/CommonService/models/exectuorService.model';
+import ExecutorServiceModel from '@/modules/CommonService/models/executorService.model';
 import SudoryWebhookModel from '@/modules/CommonService/models/sudoryWebhook.model';
 import ExportersModel from '@/modules/Exporters/models/exporters.model';
 import TokenModel from '@/modules/Token/token.model';
