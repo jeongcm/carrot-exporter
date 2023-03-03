c6b94e498d344f2ad8e1ae422f74127e5081d998
jeongcm <jeongcm0101@gmail.com>
2023-03-03 13:47:05 +0900

    improvement/1372 불필요한 코드 제거

--- src/modules/Resources/controllers/resource.controller.ts
+++ src/modules/Resources/controllers/resource.controller.ts
@@ -482,24 +482,6 @@ class ResourceController {
    * @param  {Response} res
    * @param  {NextFunction} next
    */
-  public syncResourceStatus = async (req: IRequestWithUser, res: Response, next: NextFunction) => {
-    try {
-      const clusterUuid = req.body.clusterUuid;
-      const cronTab = req.body.cronTab || config.resourceCron;
-
-      const serviceOutput: any = await this.resourceService.syncResourceStatus(clusterUuid);
-      if (!serviceOutput) res.status(404).json({ data: serviceOutput, message: `Unable to process request` });
-      res.status(200).json({ Data: serviceOutput, message: `Sync resource Successful. ${clusterUuid}, ${cronTab}` });
-    } catch (error) {
-      next(error);
-    }
-  };
-
-  /**
-   * @param  {IRequestWithUser} req
-   * @param  {Response} res
-   * @param  {NextFunction} next
-   */
   public getResourceByTypeParentCustomerAccountId = async (req: IRequestWithUser, res: Response, next: NextFunction) => {
     const resourceType: string[] = req.query.resourceType as string[];
     const parentCustomerAccountId: string = req.params.parentCustomerAccountId;
--- src/modules/Resources/routes/resource.route.ts
+++ src/modules/Resources/routes/resource.route.ts
@@ -128,14 +128,6 @@ class ResourceRoute implements Routes {
 
     this.router.post('/resource/upload/:resourceType', systemAuthMiddleware, this.resourceController.uploadResource)
 
-    this.router.post(
-      '/resource/syncResourceStatus',
-      systemAuthMiddleware,
-      //validationMiddleware(ExecutorUuidDto, 'body'),
-      //      createUserLogMiddleware,
-      this.resourceController.syncResourceStatus,
-    );
-
     this.router.get(
       '/resource/parentCustomerAccount/:parentCustomerAccountId',
       validationMiddleware(ResourceQueryDTO, 'query'),
