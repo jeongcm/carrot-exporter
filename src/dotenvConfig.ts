cfab7cff432b667456112fb5fa38b3ba8d7435ba
jeongcm <jeongcm0101@gmail.com>
2023-04-24 10:29:58 +0900

    agg-test-02/ 환경 변수 및 nodemon.json 수정

--- src/dotenvConfig.ts
+++ src/dotenvConfig.ts
@@ -4,10 +4,11 @@ import path from 'path';
 // Please change this type acccording to your own branch
 // for eg. mig_develope brach can be "mig"
 // so it will load mig.env
-const APP_DEV_TYPE = '.mig';
+const APP_DEV_TYPE = '';
 
 const envPath = path.resolve(__dirname, '..', `.env${APP_DEV_TYPE || ''}`);
 
+console.log(envPath)
 dotenv.config({
   path: path.resolve(envPath),
 });
