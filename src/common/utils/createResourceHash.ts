79204d5b077854343a583d5052acf84d3c68e997
jeongcm <jeongcm0101@gmail.com>
2023-04-24 10:27:31 +0900

    agg-test-02 불필요한 파일 제거

--- src/common/utils/createResourceHash.ts
+++
@@ -1,8 +0,0 @@
-import { IResource } from '../interfaces/resource.interface';
-import { IResourceGroup } from '../interfaces/resourceGroup.interface';
-
-const createResourceHash = (resourceGroup: IResourceGroup, resource: IResource) => {
-  return `${resource.resourceType}:${resourceGroup.resourceGroupUuid}:${resource.resourceNamespace}:${resource.resourceName}`;
-};
-
-export default createResourceHash;
