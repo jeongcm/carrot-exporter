a3d5ca67b85d2c04a72a3b67b520590e5f0b7249
jeongcm <jeongcm0101@gmail.com>
2023-04-26 13:55:05 +0900

    develop/ mqcomm sync

--- src/modules/Resources/query/query.ts
+++ src/modules/Resources/query/query.ts
@@ -1,4 +1,6 @@
 import getRegionListQuery from "@modules/Resources/query/ncp/region/region";
+import getNetworkInterfaceListQuery from "@modules/Resources/query/ncp/networkInterface/networkInterface";
+import getServerInstanceListQuery from "@modules/Resources/query/ncp/serverInstance/serverInstance";
 
 class QueryService {
 
@@ -8,6 +10,12 @@ class QueryService {
     switch (totalMsg.template_uuid) {
       case "70000000000000000000000000000001":
         queryResult = await getRegionListQuery(result, clusterUuid)
+        break;
+      case "70000000000000000000000000000003":
+        queryResult = await getNetworkInterfaceListQuery(result, clusterUuid)
+        break;
+      case "70000000000000000000000000000004":
+        queryResult = await getServerInstanceListQuery(result, clusterUuid)
         break;
     }
 
