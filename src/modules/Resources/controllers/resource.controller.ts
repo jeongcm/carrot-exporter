9f06fb1584b1122c3c748dce525a9bea8ca50317
jeongcm <jeongcm0101@gmail.com>
2023-05-02 10:34:29 +0900

    develop/ resourceEvent porting

--- src/modules/Resources/controllers/resource.controller.ts
+++ src/modules/Resources/controllers/resource.controller.ts
@@ -1,8 +1,11 @@
 import { NextFunction, Request, Response } from 'express';
 import ResourceService from "@modules/Resources/services/resource.service";
+import ResourceEventService from "@modules/Resources/services/resourceEvent.service";
+import { IRequestWithSystem } from "@common/interfaces/party.interface";
 
 class ResourceController {
   public resourceService = new ResourceService()
+  public resourceEventService = new ResourceEventService();
 
   public uploadResource = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -13,6 +16,22 @@ class ResourceController {
       next(err)
     }
   }
+
+  public uploadResourceEvent = async (req: IRequestWithSystem, res: Response, next: NextFunction) => {
+    try {
+      const resourceEventMassFeed = req.body;
+
+      const massFeedResult = await this.resourceEventService.uploadResourceEvent(resourceEventMassFeed);
+
+      if (!massFeedResult) {
+        return res.sendStatus(500);
+      }
+
+      res.status(200).json({ data: massFeedResult, message: `ResourceEvents - bulk data feed is successfully complete` });
+    } catch (error) {
+      next(error);
+    }
+  };
 }
 
 export default ResourceController;
