db71d2e362f95d91aac45673e015ae5cd22aca22
jeongcm <jeongcm0101@gmail.com>
2023-05-12 09:18:18 +0900

    pjtask/1590 cloud db metric 관련 query 개발

---
+++ src/modules/Metric/query/ncp/cloudMongoDB.ts
@@ -0,0 +1,34 @@
+export default async function getCloudMongoDBInstanceMetric(totalMsg) {
+  // initialize result
+  let queryResult = {
+    service_name: totalMsg.service_name,
+    cluster_uuid: totalMsg.cluster_uuid,
+    result: {
+      result: []
+    }
+  }
+
+  // get origin metric data
+  totalMsg.result[0].forEach((cloudMongoDBInstance) => {
+    cloudMongoDBInstance.outputs.forEach((output) => {
+      output.dps.forEach((dp) => {
+        queryResult.result.result.push(
+          {
+            metric: {
+              aggregation: output.aggregation,
+              instanceNo: output.dimensions.instanceNo,
+              interval: output.interval,
+              __name__: `co_ncp_${output.metric}`,
+              metric: output.metric,
+              productName: output.productName
+            },
+            value: [dp[0], dp[1]]
+          }
+        )
+      })
+    })
+  })
+
+
+  return queryResult
+}
