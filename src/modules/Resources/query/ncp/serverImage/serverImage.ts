7fee5205aec21142504fcd9b5b079a70cd9f97a6
jhjo <jojo@claion.co.kr>
2023-05-24 16:50:35 +0900

    Resource - 서버 이미지 상품 등록

--- src/modules/Resources/query/ncp/serverImage/serverImage.ts
+++ src/modules/Resources/query/ncp/serverImage/serverImage.ts
@@ -1,38 +1,38 @@
-import formatter_resource from "@common/utils/formatter";
+import formatter_resource from '@common/utils/formatter';
 
 export default async function getServerImageProductListQuery(result, clusterUuid) {
-    let query = {};
-    let mergedQuery = {};
-    let tempQuery = {};
+  const query = {};
+  let mergedQuery = {};
+  let tempQuery = {};
 
-    let resourceType = "SIMG";
-    let resultLength = result.getServerImageProductListResponse?.productList?.length
-    for (let i = 0; i < resultLength; i ++) {
-        query['resource_Type'] = resourceType;
-        query['resource_Spec'] = result.getServerImageProductListResponse?.productList[i];
-        query['resource_Group_Uuid'] = clusterUuid;
-        query['resource_Name'] = result.getServerImageProductListResponse?.productList[i]?.productName;
-        query['resource_Description'] = result.getServerImageProductListResponse?.productList[i]?.productDescription;
-        // query['resource_Instance'] =
-        query['resource_Target_Uuid'] = result.getServerImageProductListResponse?.productList[i]?.productCode; // new generate target uuid
-        query['resource_Target_Created_At'] = new Date();
-        // query['resource_Namespace'] =
-        // query['parent_Resource_Id'] =
-        // query['resource_Status'] =
-        query['resource_Level1'] = "NCP";
-        query['resource_Level2'] = "RG";
-        query['resource_Level3'] = "VPC";
-        query['resource_Level4'] = "SBN";
-        query['resource_Level5'] = resourceType;
-        query['resource_Level_Type'] = "NX";
-        query['resource_Rbac'] = false;
-        query['resource_Anomaly_Monitor'] = false;
-        query['resource_Active'] = true;
-        query['resource_Status_Updated_At'] = new Date();
+  const resourceType = 'SIMG';
+  const resultLength = result.getServerImageProductListResponse?.productList?.length;
+  for (let i = 0; i < resultLength; i++) {
+    query['resource_Type'] = resourceType;
+    query['resource_Spec'] = result.getServerImageProductListResponse?.productList[i];
+    query['resource_Group_Uuid'] = clusterUuid;
+    query['resource_Name'] = result.getServerImageProductListResponse?.productList[i]?.productName;
+    query['resource_Description'] = result.getServerImageProductListResponse?.productList[i]?.productDescription;
+    // query['resource_Instance'] =
+    query['resource_Target_Uuid'] = result.getServerImageProductListResponse?.productList[i]?.productCode; // new generate target uuid
+    query['resource_Target_Created_At'] = new Date();
+    // query['resource_Namespace'] =
+    // query['parent_Resource_Id'] =
+    // query['resource_Status'] =
+    query['resource_Level1'] = 'NCP';
+    query['resource_Level2'] = 'RG';
+    query['resource_Level3'] = 'VPC';
+    query['resource_Level4'] = 'SBN';
+    query['resource_Level5'] = resourceType;
+    // query['resource_Level_Type'] = 'NX';
+    query['resource_Rbac'] = false;
+    query['resource_Anomaly_Monitor'] = false;
+    query['resource_Active'] = true;
+    query['resource_Status_Updated_At'] = new Date();
 
-        tempQuery = formatter_resource(i, resultLength, resourceType, clusterUuid, query, mergedQuery);
-        mergedQuery = tempQuery;
-    }
+    tempQuery = formatter_resource(i, resultLength, resourceType, clusterUuid, query, mergedQuery);
+    mergedQuery = tempQuery;
+  }
 
-    return { message: mergedQuery, resourceType: resourceType }
+  return { message: mergedQuery, resourceType: resourceType };
 }
