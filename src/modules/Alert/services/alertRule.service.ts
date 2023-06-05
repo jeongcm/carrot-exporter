b87a602f48955943fee78714d1eb5bfb9b3914cc
jeongcm <jeongcm0101@gmail.com>
2023-06-05 15:26:14 +0900

    improvement/1853 rule, received query 수정

--- src/modules/Alert/services/alertRule.service.ts
+++ src/modules/Alert/services/alertRule.service.ts
@@ -4,6 +4,7 @@ import { IResourceGroup } from "@common/interfaces/resourceGroup.interface";
 import sequelize, { Op, where } from "sequelize";
 import * as crypto from "crypto";
 import config from "@/config";
+import mysql from "mysql2/promise";
 
 class AlertRuleService {
 
@@ -106,9 +107,9 @@ class AlertRuleService {
       let prevAlertRuleUniqueSet: any = {}
       prevAlertRules.forEach(prevAlertRule => prevAlertRuleUniqueSet[prevAlertRule.SHA1] = prevAlertRule.id)
 
-      let prevAlertReceives: any
-      let prevAlertReceivedSet: any = {}
-      let prevAlertReceivedIntersectionSet: any = {}
+      let prevAlertReceives: any // 이전에만 존재하던 alertReceived list
+      let prevAlertReceivedSet: any = {} // 이전에만 존재하는 alertReceived map
+      let prevAlertReceivedIntersectionSet: any = {} // 이전과 새로운 데이터 모두 존재하는 alertReceived map
       if (prevAlertRuleUniqueSet.length > 0) {
         let prevAlertRuleIds = prevAlertRuleUniqueSet.map(ar => ar.id)
 
@@ -117,43 +118,42 @@ class AlertRuleService {
           alertRuleKey: {[Op.in]: prevAlertRuleIds }
         }})
 
-        if (prevAlertReceives.length > 0) {
-          // 이전 alertReceived 가 존재하면 alertReceived hashing 해서 key value 로 저장
-          for (let prevAlertReceived of prevAlertReceives) {
-            let hash = await this.alertReceivedToSHA1(prevAlertReceived)
-            prevAlertReceivedSet[hash] = prevAlertReceived;
-          }
+        // 이전 alertReceived 가 존재하면 alertReceived hashing 해서 key value 로 저장
+        for (let prevAlertReceived of prevAlertReceives) {
+          let hash = await this.alertReceivedToSHA1(prevAlertReceived)
+          prevAlertReceivedSet[hash] = prevAlertReceived;
         }
       }
 
-      let alertRuleSet: any = {}
+      let alertRuleSet: any = {} // 새로 들어온 alertRule map
       let newAlertReceivedSet: any = {}
-      for (let group of result.Groups) {
-        for (let rule of group.Rules) {
+      for (let group of result.groups) {
+        for (let rule of group.rules) {
           if (rule.Labels["serverity"] === "none" || rule.Labels["serverity"] === "") {
             continue
           }
 
-          let alertRule = this.getAlertRuleQuery(clusterUuid, group.Name, resourceGroup.customerAccountKey, rule)
-          let alertRuleHash = await this.alertRuleToSHA1(alertRule)
+          let alertRule = this.getAlertRuleQuery(clusterUuid, group.name, resourceGroup.customerAccountKey, rule)
+          let alertRuleHash = await this.alertRuleToSHA1(alertRule) // make hash
 
-          alertRule["alert_rule_key"] = this.getAlertRuleKey(prevAlertRuleUniqueSet, alertRuleHash) || 0
-          alertRule["updated_by"] = config.partyUser.userId
+          alertRule["alert_rule_key"] = this.getAlertRuleKey(prevAlertRuleUniqueSet, alertRuleHash) || 0 // alertRuleKey setting
+          alertRule["updated_by"] = config.partyUser.userId // editor
           alertRuleSet[alertRuleHash] = alertRule
           // alertReceived
 
           for (let alert of rule.Alerts) {
-            let alertReceived = this.getAlertReceivedQuery(alertRule["alert_rule_key"], resourceGroup.customerAccountKey, alert)
+            let alertReceived = this.getAlertReceivedQuery(alertRule["alert_rule_key"], resourceGroup.customerAccountKey, alert) // get alertReceived query
 
-            let alertReceivedHash = await this.alertReceivedToSHA1(alertReceived)
-            alertReceived["updated_by"] = config.partyUser.userId
+            let alertReceivedHash = await this.alertReceivedToSHA1(alertReceived) // make alertReceived hash
+            alertReceived["updated_by"] = config.partyUser.userId // editor
 
+            // 이전 alertReceived에 존재한다면
             if (prevAlertReceivedSet[alertReceivedHash]) {
-              // 중복 얼럿 추가
+              // 이전과 새로운 데이터를 모두 가진 alertReceived map에 얼럿 추가
               prevAlertReceivedIntersectionSet[alertReceivedHash] = prevAlertReceivedSet[alertReceivedHash]
             }
 
-            // 이전 얼럿 삭제
+            // 모두 가진 alertReceived map에 넣었기 때문에 이전 얼럿 삭제
             delete prevAlertReceivedSet[alertReceivedHash]
 
             newAlertReceivedSet[alertRuleHash][alertReceivedHash] = alertReceived
@@ -162,7 +162,29 @@ class AlertRuleService {
       }
 
       // 2. upsert to database
-
+      // const mysqlConnection = await mysql.createConnection({
+      //   host: config.db.mariadb.host,
+      //   user: config.db.mariadb.user,
+      //   port: config.db.mariadb.port || 3306,
+      //   password: config.db.mariadb.password,
+      //   database: config.db.mariadb.dbName,
+      //   multipleStatements: true,
+      // });
+      // await mysqlConnection.query('START TRANSACTION');
+      // try {
+      //   prevAlertRuleUniqueSet.map()
+      //
+      //
+      //
+      //   await mysqlConnection.query(query1, [query2]);
+      //   await mysqlConnection.query('COMMIT');
+      // } catch (err) {
+      //   await mysqlConnection.query('ROLLBACK');
+      //   await mysqlConnection.end();
+      //   console.info('Rollback successful');
+      // }
+      //
+      // await mysqlConnection.end();
 
     } catch (err) {
       console.log(`failed to processAlertRule. cause: ${err}`)
@@ -173,25 +195,20 @@ class AlertRuleService {
   private getAlertRuleQuery(clusterUuid, groupName, customerAccountKey, rule): object {
     let ruleQuery = {}
 
-    ruleQuery['created_by'] =
-    ruleQuery['updated_by'] =
-    ruleQuery['created_at'] =
-    ruleQuery['updated_at'] =
-    ruleQuery['deleted_at'] =
-    ruleQuery['alert_rule_name'] = rule.Name
+    ruleQuery['alert_rule_name'] = rule.name
     ruleQuery['alert_rule_group'] = groupName
-    ruleQuery['alert_rule_query'] = rule.Query
-    ruleQuery['alert_rule_duration'] = rule.Duration.toInt()
-    ruleQuery['alert_rule_severity'] = rule.Labels
-    ruleQuery['alert_rule_description'] = rule.Annotations["description"]
-    ruleQuery['alert_rule_runbook'] = rule.Annotations["runbook_url"]
-    ruleQuery['alert_rule_summary'] = rule.Annotations["summary"]
-    ruleQuery['alert_rule_state'] = rule.State
+    ruleQuery['alert_rule_query'] = rule.query
+    ruleQuery['alert_rule_duration'] = rule.duration.toInt()
+    ruleQuery['alert_rule_severity'] = rule.labels["severity"]
+    ruleQuery['alert_rule_description'] = rule.annotations["description"]
+    ruleQuery['alert_rule_runbook'] = rule.annotations["runbook_url"]
+    ruleQuery['alert_rule_summary'] = rule.annotations["summary"]
+    ruleQuery['alert_rule_state'] = rule.state
     ruleQuery['customer_account_key'] = customerAccountKey
     ruleQuery['resource_group_uuid'] = clusterUuid
-    ruleQuery['alert_rule_health'] = rule.Health.toString()
-    ruleQuery['alert_rule_evaluation_time'] = rule.EvaluationTime
-    ruleQuery['alert_rule_last_evaluation'] = rule.LastEvaluation
+    ruleQuery['alert_rule_health'] = rule.health
+    ruleQuery['alert_rule_evaluation_time'] = rule.evaluationTime
+    ruleQuery['alert_rule_last_evaluation'] = rule.lastEvaluation
 
     return ruleQuery
   }
@@ -199,27 +216,27 @@ class AlertRuleService {
   private getAlertReceivedQuery(ruleKey, customerAccountKey, received): object {
     let receivedQuery = {}
 
-    receivedQuery['alert_received_name'] = received.Labels["alertname"]
-    receivedQuery['alert_received_severity'] = received.Labels["severity"]
-    receivedQuery['alert_received_state'] = received.state.toString()
-    receivedQuery['alert_received_description'] = received.Labels["description"]
-    receivedQuery['alert_received_summary'] = received.Labels["summary"]
+    receivedQuery['alert_received_name'] = received.labels["alertname"]
+    receivedQuery['alert_received_severity'] = received.labels["severity"]
+    receivedQuery['alert_received_state'] = received.state
+    receivedQuery['alert_received_description'] = received.annotaions["description"]
+    receivedQuery['alert_received_summary'] = received.annotaions["summary"]
     receivedQuery['alert_received_active_at'] = received.activedAt
     receivedQuery['alert_received_value'] = parseFloat(received.value)
-    receivedQuery['alert_received_namespace'] = received.Labels["namespace"]
-    receivedQuery['alert_received_node'] = received.Labels["node"]
-    receivedQuery['alert_received_service'] = received.Labels["service"]
-    receivedQuery['alert_received_pod'] = received.Labels["pod"]
-    receivedQuery['alert_received_persistentvolumeclaim'] = received.Labels["persistentvolumeclaim"]
+    receivedQuery['alert_received_namespace'] = received.labels["namespace"]
+    receivedQuery['alert_received_node'] = received.labels["node"]
+    receivedQuery['alert_received_service'] = received.labels["service"]
+    receivedQuery['alert_received_pod'] = received.labels["pod"]
+    receivedQuery['alert_received_persistentvolumeclaim'] = received.labels["persistentvolumeclaim"]
     receivedQuery['alert_received_pinned'] = 0
     receivedQuery['customer_account_key'] = customerAccountKey
     receivedQuery['alert_rule_key'] = ruleKey
-    receivedQuery['alert_received_instance'] = received.Labels["instance"]
-    receivedQuery['alert_received_labels'] = received.Labels
-    receivedQuery['alert_received_container'] = received.Labels["container"]
-    receivedQuery['alert_received_endpoint'] = received.Labels["endpoint"]
-    receivedQuery['alert_received_reason'] = received.Labels["reason"]
-    receivedQuery['alert_received_uid'] = received.Labels["uid"]
+    receivedQuery['alert_received_instance'] = received.labels["instance"]
+    receivedQuery['alert_received_labels'] = received.labels
+    receivedQuery['alert_received_container'] = received.labels["container"]
+    receivedQuery['alert_received_endpoint'] = received.labels["endpoint"]
+    receivedQuery['alert_received_reason'] = received.labels["reason"]
+    receivedQuery['alert_received_uid'] = received.labels["uid"]
     receivedQuery['alert_received_affected_resource_type'] = "NA";
     receivedQuery['alert_received_affected_resource_name'] = "NA";
 
