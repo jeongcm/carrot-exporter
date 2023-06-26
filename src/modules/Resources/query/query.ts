e4c8a4ad2c848dc3562458b7d8b795b4a850d15a
jhjo <jojo@claion.co.kr>
2023-06-26 17:34:16 +0900

    Resource NatGateway 추가

--- src/modules/Resources/query/query.ts
+++ src/modules/Resources/query/query.ts
@@ -49,6 +49,7 @@ import getZoneListQuery from '@modules/Resources/query/ncp/zone/zone';
 import getDemandCostQuery from '@modules/Cost/query/ncp/demandCost';
 import getNetworkAclListQuery from '@modules/Resources/query/ncp/networkAcl/networkAcl';
 import getCostRelationCodeList from '@modules/Cost/query/ncp/costRelationCode';
+import getNatGatewayListQuery from '@modules/Resources/query/ncp/natGateway/natGateway';
 
 class QueryService {
   public cloudDBMysqlService = new CloudDBMysqlService();
@@ -159,6 +160,7 @@ class QueryService {
       case '70000000000000000000000000000041':
       case 'NCM00000000000000000000000000019':
       case '70000000000000000000000000000028':
+      case '70000000000000000000000000000017':
         queryResult = await this.getNcpResourceQuery(totalMsg);
         break;
       default:
@@ -217,6 +219,9 @@ class QueryService {
         break;
       case '70000000000000000000000000000015':
         queryResult = await getVpcListQuery(result, clusterUuid);
+        break;
+      case '70000000000000000000000000000017':
+        queryResult = await getNatGatewayListQuery(result, clusterUuid);
         break;
       case '70000000000000000000000000000028':
         queryResult = await getTargetGroupListQuery(result, clusterUuid);
