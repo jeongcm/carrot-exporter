5e41c5f0826fe030af74f3e67b2ee50756020be4
jeongcm <jeongcm0101@gmail.com>
2023-06-29 15:58:54 +0900

    improvement/1853 rollback

--- src/modules/Alert/services/alertRule.service.ts
+++ src/modules/Alert/services/alertRule.service.ts
@@ -390,7 +390,7 @@ class AlertRuleService {
     receivedQuery['alertReceivedDescription'] = received.annotations?.description || ''
     receivedQuery['alertReceivedSummary'] = received.annotations?.summary || ''
     receivedQuery['alertReceivedActiveAt'] = received.activeAt
-    receivedQuery['alertReceivedValue'] = received.value.toFixed(2) + '%'
+    receivedQuery['alertReceivedValue'] = received.value
     receivedQuery['alertReceivedNamespace'] = received.labels["namespace"]
     receivedQuery['alertReceivedNode'] = received.labels["node"] || ''
     receivedQuery['alertReceivedService'] = received.labels["service"] || ''
