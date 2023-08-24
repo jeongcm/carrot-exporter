1225836e2080bfc3d87444be594ccc6c14682c3f
jeongcm <jeongcm0101@gmail.com>
2023-08-24 15:04:10 +0900

    develop / update deployment, env

--- src/config/index.ts
+++ src/config/index.ts
@@ -15,7 +15,7 @@ export default {
   appPort: process.env.CM_EXPORTER_PORT || 6001,
   appUrl: process.env.CM_EXPORTER_URL || 'http://localhost',
   nodeEnv: process.env.CM_EXPORTER_ENV || 'development',
-  maxApiBodySize: process.env.CO_MAX_API_BODY_SIZE || '50mb',
+  maxApiBodySize: process.env.CM_EXPORTER_MAX_API_BODY_SIZE || '50mb',
   logFormat: process.env.CM_EXPORTER_LOG_FORMAT,
   coApi: {
     url: process.env.API_URL || 'http://localhost',
@@ -45,31 +45,31 @@ export default {
 
   victoriaMetrics: {
     CM_EXPORTER_VM_ADDRESS: process.env.CM_EXPORTER_VM_ADDRESS,
-    CO_VM_SINGLE_ADDRESS: process.env.CO_VM_SINGLE_ADDRESS,
+    CM_EXPORTER_VM_SINGLE_ADDRESS: process.env.CM_EXPORTER_VM_SINGLE_ADDRESS,
     CM_EXPORTER_VM_API: process.env.CM_EXPORTER_VM_API,
     vmSingleUrl: process.env.CM_EXPORTER_VM_SINGLE_ADDRESS || 'http://localhost',
     vmMultiUrl: process.env.CM_EXPORTER_VM_MULTI_ADDRESS || 'http://localhost',
     vmImport: process.env.CM_EXPORTER_VM_IMPORT || '/api/v1/import?extra_label=clusterUuid=',
     vmMultiBaseUrlInsert:
-      process.env.CO_VM_MULTI_BASE_URL_INSERT || 'http://localhost',
+      process.env.CM_EXPORTER_VM_MULTI_BASE_URL_INSERT || 'http://localhost',
     vmMultiBaseUrlSelect:
-      process.env.CO_VM_MULTI_BASE_URL_SELECT || 'http://localhost',
-    vmMultiAuthUrl: process.env.CO_VM_MULTI_AUTH_URL || 'http://localhost',
-    vmMultiNamespaces: process.env.CO_VM_MULTI_NAMESPACE || 'vm-multi-tenant',
-    vmMultiSecret: process.env.CO_VM_MULTI_SECRET || 'vm-auth-victoria-metrics-auth',
-    vmMultiClusterUuid: process.env.CO_VM_MULTI_CLUSTER_UUID,
+      process.env.CM_EXPORTER_VM_MULTI_BASE_URL_SELECT || 'http://localhost',
+    vmMultiAuthUrl: process.env.CM_EXPORTER_VM_MULTI_AUTH_URL || 'http://localhost',
+    vmMultiNamespaces: process.env.CM_EXPORTER_VM_MULTI_NAMESPACE || 'vm-multi-tenant',
+    vmMultiSecret: process.env.CM_EXPORTER_VM_MULTI_SECRET || 'vm-auth-victoria-metrics-auth',
+    vmMultiClusterUuid: process.env.CM_EXPORTER_VM_MULTI_CLUSTER_UUID,
     vmOption: process.env.CM_EXPORTER_VM_OPTION || 'SINGLE',
-    vmOpenstackSwitch: process.env.CO_VM_OPENSTACK_SWITCH || 'off',
+    vmOpenstackSwitch: process.env.CM_EXPORTER_VM_OPENSTACK_SWITCH || 'off',
   },
 
   obsUrl: {
-    kpsNamespace: process.env.CO_KPS_NAMESPACE || 'monitor',
-    prometheusUrlHead: process.env.CO_PROMETHEUS_URL_HEAD || 'http://kps-kube-prometheus-stack-prometheus.',
-    prometheusUrlTail: process.env.CO_PROMETHEUS_URL_TAIL || '.svc.cluster.local:9090',
+    kpsNamespace: process.env.CM_EXPORTER_KPS_NAMESPACE || 'monitor',
+    prometheusUrlHead: process.env.CM_EXPORTER_PROMETHEUS_URL_HEAD || 'http://kps-kube-prometheus-stack-prometheus.',
+    prometheusUrlTail: process.env.CM_EXPORTER_PROMETHEUS_URL_TAIL || '.svc.cluster.local:9090',
     nodeExporterPort: process.env.NODE_EXPORTER_PORT || 9100,
   },
   oT: {
-    oTTraceLogTurnOff: process.env.CO_TURN_OFF_TELEMETRY,
+    oTTraceLogTurnOff: process.env.CM_EXPORTER_TURN_OFF_TELEMETRY,
   },
 
   partyUser: {
@@ -77,7 +77,7 @@ export default {
   },
 
   alert: {
-    alertExpiredDate: process.env.CO_ALERT_EXPIRED_DATE || 60 * 60 * 24 // 1d
+    alertExpiredDate: process.env.CM_EXPORTER_ALERT_EXPIRED_DATE || 60 * 60 * 24 // 1d
   },
 
   metricMeta: {
